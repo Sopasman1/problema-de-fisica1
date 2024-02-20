7a77f5bb9b3d263302a177e348e45193f1f558a7
Jezuz <Jezuz@DESKTOP-RN4DP0B>
2024-02-20 01:35:11 -0600

    Agregar archivos de proyecto.

---
+++ problema de fisica1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace problema_de_fisica1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            densidad deresult=new densidad();
+            Console.WriteLine("por favor coloca los kilogramos");
+            deresult.kilos = double.Parse(Console.ReadLine());
+            Console.WriteLine("coloca los metros3");
+            deresult.metros3 = double.Parse(Console.ReadLine());
+            deresult.resultado(deresult.kilos,deresult.metros3);
+            Console.WriteLine("densidad =" + deresult.kilos / deresult.metros3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ problema de fisica1/masa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_de_fisica1
+{
+    internal class densidad
+    {
+        public double kilos;
+        public double metros3;
+
+        public double Kilos { get => kilos; set => kilos = value; }
+        public double Metros3 { get => metros3; set => metros3 = value; }
+
+        public void resultado(double kilos,double metros3)
+        {
+            this.kilos = kilos;
+            this.metros3 = metros3;
+        }
+    }
+}
